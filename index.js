c5df0ed5761405dd582c10176265e555b5714568
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-19 17:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg);
 }
+
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg);
+}
